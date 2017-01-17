6baaf27ac907e14222a2d6329ea8e800ab39fa49
shtigran <shakhbekyantigran@gmail.com>
2017-01-17 12:46:21 +0400

    Replace Armenian text into English

---
+++ ReplaceArmText/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReplaceArmText;
+
+namespace Program
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string str = "Բարև Հայեր ջան, 1000 բարի, ինչպես եք?";
+      Console.WriteLine(str.ReplaceArmText());
+      
+      Console.ReadKey();
+
+    }
+  }
+}
